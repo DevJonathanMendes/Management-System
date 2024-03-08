ad7c6f30a176d5fa38a27d126a41f5e57bee822a
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-03-08 15:11:06 -0300

    feat: add cors, helmet and compression

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  app.use(compression());
+
   app.setGlobalPrefix('/api');
+
   await app.listen(3000);
 }
 bootstrap();

caf8282add7066e242acc78e5f61da7bba65ba10
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-05-16 13:36:17 -0300

    refactor: add FormLayout

--- apps/frontend/src/pages/SignIn.tsx
+++ apps/frontend/src/pages/SignIn.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Button, Container, Form, Stack } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import API from "../api/fetch";
 import { FormInput } from "../components/forms/FormInput";
 import { useAuth } from "../hooks/useAuth";
 import { useForm } from "../hooks/useForm";
+import { FormLayout } from "../layouts/FormLayout";
 
 export const SignInPage: React.FC = () => {
   const { signIn } = useAuth();
@@ -22,34 +23,30 @@ export const SignInPage: React.FC = () => {
   };
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit}>
-        <Stack gap={4}>
-          <h1>Login to your account</h1>
-          <FormInput
-            label="Username"
-            type="text"
-            value={username}
-            setValue={setUsername}
-            placeholder="Enter username"
-            required
-          />
-          <FormInput
-            label="Password"
-            type="password"
-            value={password}
-            setValue={setPassword}
-            placeholder="Enter password"
-            required
-          />
-          <Button type="submit">Sign In</Button>
-          <p className="text-center">
-            <small>
-              <Link to="/signup">Don't have an account?</Link>
-            </small>
-          </p>
-        </Stack>
-      </Form>
-    </Container>
+    <FormLayout onSubmit={handleSubmit}>
+      <h1>Login to your account</h1>
+      <FormInput
+        label="Username"
+        type="text"
+        value={username}
+        onChange={setUsername}
+        placeholder="Enter username"
+        required
+      />
+      <FormInput
+        label="Password"
+        type="password"
+        value={password}
+        onChange={setPassword}
+        placeholder="Enter password"
+        required
+      />
+      <Button type="submit">Sign In</Button>
+      <p className="text-center">
+        <small>
+          <Link to="/signup">Don't have an account?</Link>
+        </small>
+      </p>
+    </FormLayout>
   );
 };
--- apps/frontend/src/pages/SignUp.tsx
+++ apps/frontend/src/pages/SignUp.tsx
@@ -1,14 +1,15 @@
-import { Button, Container, Form, Stack } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import API from "../api/fetch";
 import { FormInput } from "../components/forms/FormInput";
 import { useAuth } from "../hooks/useAuth";
 import { useForm } from "../hooks/useForm";
+import { FormLayout } from "../layouts/FormLayout";
 
 export const SignUpPage = () => {
   const { signIn } = useAuth();
-  const {
+  const { 
     username, setUsername,
     email, setEmail,
     password, setPassword
@@ -22,43 +23,39 @@ export const SignUpPage = () => {
   };
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit}>
-        <Stack gap={4}>
-          <h1>Create your account</h1>
-          <FormInput
-            label="Username"
-            type="text"
-            value={username}
-            setValue={setUsername}
-            placeholder="Enter username"
-            required
-          />
-          <FormInput
-            label="Email"
-            type="email"
-            value={email}
-            setValue={setEmail}
-            placeholder="Enter e-mail"
-            required
-          />
-          <FormInput
-            label="Password"
-            type="password"
-            value={password}
-            setValue={setPassword}
-            placeholder="Enter password"
-            required
-          />
+    <FormLayout onSubmit={handleSubmit}>
+      <h1>Create your account</h1>
+      <FormInput
+        label="Username"
+        type="text"
+        value={username}
+        onChange={setUsername}
+        placeholder="Enter username"
+        required
+      />
+      <FormInput
+        label="Email"
+        type="email"
+        value={email}
+        onChange={setEmail}
+        placeholder="Enter e-mail"
+        required
+      />
+      <FormInput
+        label="Password"
+        type="password"
+        value={password}
+        onChange={setPassword}
+        placeholder="Enter password"
+        required
+      />
 
-          <Button type="submit">Sing Up</Button>
-          <p className="text-center">
-            <small>
-              <Link to="/signin">Already have an account?</Link>
-            </small>
-          </p>
-        </Stack>
-      </Form>
-    </Container>
+      <Button type="submit">Sing Up</Button>
+      <p className="text-center">
+        <small>
+          <Link to="/signin">Already have an account?</Link>
+        </small>
+      </p>
+    </FormLayout>
   );
 };
